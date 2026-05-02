842643b12d1355cd5df372529bae43ac4ab50ee2
KhandakarShakilRana <khandakarshakilrana770@gmail.com>
2026-05-02 08:13:26 +0600

    animal rendaering

--- src/app/animals/[id]/page.jsx
+++ src/app/animals/[id]/page.jsx
@@ -4,15 +4,25 @@ import AnimalProvider, { AnimalContext } from "@/context/AnimalContext";
 import { useParams } from "next/navigation";
 import React, { use, useContext } from "react";
 
-
 const AnimalDetailPage = () => {
-    const {id} = useParams(); 
-  const {data} = useContext(AnimalContext)
-  const animal = data.find((a) => a.id == id)
-  console.log(animal);
-  return <div>
-    
-  </div>;
+  const { id } = useParams();
+  const { data } = useContext(AnimalContext);
+  const animal = data.find((a) => a.id === Number(id));
+
+  if (!animal) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <div>
+          <img src={`${animal.image}`} alt="" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default AnimalDetailPage;
--- src/components/Animals.jsx
+++ src/components/Animals.jsx
@@ -10,15 +10,14 @@ const Animals = async () => {
       <h1 className="text-[#213D34] text-center font-bold mt-10 text-4xl ">
         Featured Animals
       </h1>
-      <div className="container mx-auto grid grid-cols-3 gap-6 py-20">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6 py-20">
         {animals.map((animal) => (
           <div key={animal.id} className="text-[#213D34]">
-            <div className="w-100 mx-auto p-4 rounded-2xl bg-white shadow-xl ">
+            <div className=" mx-auto p-4 rounded-2xl bg-white shadow-xl ">
               <img
                 src={`${animal.image}`}
                 alt="Deshi Shahi Cow"
-                width={360}
-                className="rounded-2xl"
+                className="rounded-2xl w-full"
               />
               <div className="flex items-center justify-between">
                 <h1 className="text-[#213D34] text-2xl font-semibold my-3 ">
@@ -26,13 +25,15 @@ const Animals = async () => {
                 </h1>
                 <h1 className="text-2xl font-bold">{animal.price}TK</h1>
               </div>
-              <div className="flex justify-between items-center w-fyll">
-                  <h1>Type</h1>
-                  <h1>from</h1>
+              <div className="flex justify-between items-center w-full mb-4 ">
+                  <h1>Type : {animal.type}</h1>
+                  <h1>From : {animal.location}</h1>
               </div>
-              <Link href={`animals/${animal.id}`}>
-                <button className="btn text-black bg-[#D9EF78]">Deatil</button>
+              <div className="text-center">
+                <Link href={`animals/${animal.id}`}>
+                <button className="btn text-white bg-[#213D34]">View Deatil</button>
               </Link>
+              </div>
             </div>
           </div>
         ))}
